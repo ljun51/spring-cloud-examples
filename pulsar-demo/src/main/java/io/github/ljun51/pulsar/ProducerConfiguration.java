363592a37365e868dbf72bf9673b3b26b7797f74
ljun51 <ljun51@outlook.com>
2023-01-03 17:10:48 +0800

    add pulsar demo

--- pulsar-demo/src/main/java/io/github/ljun51/pulsar/ProducerConfiguration.java
+++ pulsar-demo/src/main/java/io/github/ljun51/pulsar/ProducerConfiguration.java
@@ -1,6 +1,12 @@
 package io.github.ljun51.pulsar;
 
 import io.github.majusko.pulsar.producer.ProducerFactory;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,5 +22,25 @@ public class ProducerConfiguration {
         return new ProducerFactory()
                 .addProducer("my-topic", MyMsg.class)
                 .addProducer("other-topic", String.class);
+    }
+
+    @Bean
+    @SneakyThrows
+    public PulsarClient pulsarClient() {
+        return PulsarClient.builder()
+        .serviceUrl("pulsar://localhost:6650")
+        .build();
+    }
+
+    @Bean
+    @SneakyThrows
+    public Producer<byte[]> producerClient() {
+        return pulsarClient().newProducer()
+        .topic("my-topic")
+        .producerName(null)
+        .sendTimeout(30, TimeUnit.SECONDS)
+        .blockIfQueueFull(false)
+        .maxPendingMessages(1000)
+        .create();
     }
 }
